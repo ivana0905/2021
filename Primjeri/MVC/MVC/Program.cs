bba61054cc1f878a0e1891cc59586839eb45b098
ivana0905 <74403847+ivana0905@users.noreply.github.com>
2021-07-06 20:24:28 +0200

    Update Program.cs

--- Primjeri/MVC/MVC/Program.cs
+++ Primjeri/MVC/MVC/Program.cs
@@ -14,23 +14,7 @@ namespace MVC
   {
     public static void Main(string[] args)
     {
-      var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-      try
-      {
-        logger.Debug("init main");
-        CreateHostBuilder(args).Build().Run();
-      }
-      catch (Exception exception)
-      {
-        //NLog: catch setup errors
-        logger.Error(exception, "Stopped program because of exception");
-        throw;
-      }
-      finally
-      {
-        // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-        NLog.LogManager.Shutdown();
-      }
+       CreateHostBuilder(args).Build().Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -38,7 +22,7 @@ namespace MVC
             .ConfigureWebHostDefaults(webBuilder =>
             {
               webBuilder.UseStartup<Startup>();
-            })
-            .UseNLog();
+            });
+           
   }
 }
